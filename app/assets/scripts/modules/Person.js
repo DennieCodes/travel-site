3ce1942ae8f033425253d88ebbbaf9b72146ee59
Dennie <oculareo@gmail.com>
2018-08-13 13:46:46 -0400

    Resolved webpack conflicts

--- app/assets/scripts/modules/Person.js
+++ app/assets/scripts/modules/Person.js
@@ -1,8 +1,9 @@
 function Person(fullName, favColor) {
-	this.name = fullName;
-	this.favoriteColor = favColor;
-
-	this.greet = function() {
-		console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor);
-	}
+  this.name = fullName;
+  this.favoriteColor = favColor;
+  this.greet = function() {
+    console.log("Hello, my name is " + this.name + " and my favorite color is " + this.favoriteColor + ".");
+  }
 }
+
+module.exports = Person;
