8842f49e6a9d098f96892be493a4a28425c8c269
Dennie <oculareo@gmail.com>
2018-08-14 16:38:54 -0400

    Implemented Classes into JS portions

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,20 +1,3 @@
-var $ = require('jquery');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-// NOTE: Below line prior to ES6
-// var Person = require('./modules/Person');
-
-class Adult extends Person {
-	payTaxes() {
-		console.log(this.name + " now owes $0 in taxes.");
-	}
-}
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "green");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
+var mobileMenu = new MobileMenu();
